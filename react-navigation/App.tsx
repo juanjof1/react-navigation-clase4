3dc65452923143ec9939b17aac7ed157cc3f6e99
juanjof1 <salazargiljuanjose@gmail.com>
2022-05-10 15:09:59 -0600

    new react

---
+++ react-navigation/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ThemeProvider } from 'react-native-elements';
+import './config/firebase';
+import RootNavigation from './navigation';
+import 'react-native-gesture-handler';
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <RootNavigation />
+    </ThemeProvider>
+  );
+}
